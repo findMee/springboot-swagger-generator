81d2d9cbe4584db49266add0701f91934b607471
user1m <user1@heroku.com>
2020-03-20 00:16:01 +0530

    Integrated swaggerFox for api documentaion

--- src/main/java/com/example/springbootswaggercrud/crud/controller/EmployeeController.java
+++ src/main/java/com/example/springbootswaggercrud/crud/controller/EmployeeController.java
@@ -7,36 +7,42 @@ import java.util.Map;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.springbootswaggercrud.crud.model.Employee;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
-@RequestMapping("v1/employee")
+@RequestMapping("employees/")
 public class EmployeeController {
 
     private Map<String, Employee> empData = new HashMap<>();
 
     @RequestMapping(method = RequestMethod.GET)
+    @ApiOperation(value = "Returns the list of Employees", response = Collection.class)
     public Collection<Employee> getEmployees() {
         return empData.values();
     }
 
     @RequestMapping(method = RequestMethod.POST)
+    @ApiOperation(value = "Adding the Employee", response = Employee.class)
     public Employee addEmployee(@RequestBody Employee employee) {
         empData.put(employee.getId(), employee);
         return employee;
     }
 
     @RequestMapping(method = RequestMethod.PUT)
+    @ApiOperation(value = "Updating the Employee", notes = "Provide the employee details to you want to update", response = Employee.class)
     public Employee updateEmployee(@RequestBody Employee employee) {
         empData.put(employee.getId(), employee);
         return employee;
     }
 
     @RequestMapping(path = "empId", method = RequestMethod.DELETE)
+    @ApiOperation(value = "Delete the Employee")
     public void deleteEmployee(@PathVariable("empId") String empId) {
         empData.remove(empId);
     }
 
     @RequestMapping(path = "empId", method = RequestMethod.GET)
+    @ApiOperation(value = "Get a particular Employee", response = Employee.class)
     public Employee getEmployee(@PathVariable("empId") String empId) {
         return empData.get(empId);
     }
--- src/main/java/com/example/springbootswaggercrud/crud/controller/HomeController.java
+++ src/main/java/com/example/springbootswaggercrud/crud/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/v1")
 public class HomeController {
 
     @RequestMapping(method = RequestMethod.GET)
